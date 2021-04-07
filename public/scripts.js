212d91d77114c4b182c893c7cb72a2889e76e955
nisaputri <pnisa1911@gmail.com>
2021-04-07 14:13:29 -0400

    Update - from the origin

--- public/scripts.js
+++ public/scripts.js
@@ -2,58 +2,31 @@ async function populateRestaurants() {
     const diningRequest = await fetch('/api/dining/');
     const diningData = await diningRequest.json();
 
-    const dinningList = [];
-    dinningList.push(diningData.data);
+    const diningList = [];
+    diningList.push(diningData.data);
 
-    diningData.data.forEach((restaurant) => {
-        const name = restaurant.hall_name;
-        const location = restaurant.hall_address;
+    const cleanedDiningList = diningList[0];
+    const targetBox = document.querySelector('#targetBox');
+
+    cleanedDiningList.forEach((index) => {
+        const diningId = index.hall_id;
+        const diningName = index.hall_name;
+        const diningLocation = index.hall_address;
 
         const appendItem = document.createElement('tr');
 
-
-        appendItem.classList.add('tile', 'has-text-centered', 'is-parent', 'us-3');
-        appendItem.innerHTML = 
-        `<td>${id}</td> <td>${name}</td> <td>${location}</td>`;
+        appendItem.innerHTML = `
+        <td>${diningId}</td> 
+        <td>${diningName}</td> 
+        <td>${diningLocation}</td>`;
 
     targetBox.append(appendItem);
     });
 }
 
-async function getMeals() {
-    const diningRequest = await fetch("/api/meals");
-    const diningData = await diningRequest.json();
-    return diningData;
-}
-
-function setBasicData() {
-    localStorage.setItem("myCat", "Tom");
-}
-
-function setComplexData(data) {
-    localStorage.setItem('data', JSON.stringify(data));
-}
-
-function getBasicData() {
-    return localStorage.getItem('myCat');
-}
-
 async function windowActions() {
-    console.log('loaded window');
-    const meals = await getMeals();
-    console.table(meals);
-
-
-    //setBasicData();
-    //const cat = getBasicData();
-
-    setComplexData(meals);
-    const storedMeals = localStorage.getItem('data');
-    const storedMealData = JSON.parse(storedMeals)
-    console.log(storedMeals);
+    populateRestaurants();
 }
-
-
 window.onload = windowActions;
 
 
